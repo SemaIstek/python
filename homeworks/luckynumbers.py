07aba6a1e27abb61d5ca89bcf57cb6ea4b4af387
Sema <sema.istek@powercoders.org>
2023-11-16 17:54:54 +0100

    Create files

---
+++ homeworks/luckynumbers.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,23,24,25,26,27,28,29,30] 
+s = 2
+l = len(numbers)
+
+while s < l:
+    new_numbers = []  
+    
+    for i in range(len(numbers)):  
+        if (i + 1) % s != 0:
+            new_numbers.append(numbers[i]) 
+    
+    numbers = new_numbers 
+    s += 1
+
+
+print("Lucky numbers are", numbers)
