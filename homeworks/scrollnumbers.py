0c87f327d90113c5f1c1163d61b2a1fcfb8303a3
Sema <sema.istek@powercoders.org>
2023-11-16 17:55:38 +0100

    Create file

--- homeworks/scrollnumbers.py
+++ homeworks/scrollnumbers.py
@@ -1,19 +1,17 @@
 def shift_list_elements(lst, n):
-    n = n % len(lst)  # Modulus alarak etkili kaydırmayı hesapla
-    shifted_list = lst[-n:] + lst[:-n]  # Listeyi kaydır
+    n = n % len(lst)  
+    shifted_list = lst[-n:] + lst[:-n]  
 
     return shifted_list
 
-# Kullanıcıdan liste girişi al
+
 input_list = input("Enter a list of numbers separated by spaces: ")
-# Girişi boşluklara göre böl ve integer'a dönüştür
+
 input_list = [int(x) for x in input_list.split()]
 
-# Kullanıcıdan kaydırma miktarı girişi al
+
 shift_value = int(input("Enter the shift value: "))
 
-# Kaydırma işlemi
-output = shift_list_elements(input_list, shift_value)
 
-# Sonucu yazdır
+output = shift_list_elements(input_list, shift_value)
 print(f"Input: {input_list}, Shift: {shift_value}, Output: {output}")
